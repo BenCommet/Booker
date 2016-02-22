6573bbd77f2e5d9cadc4fe5d097acc921939ee45
kolarikm <kolarikm@mail.gvsu.edu>
2016-02-22 12:52:32 -0500

    Added error checking for username and password fields

--- app/src/main/java/com/example/commet/booker/MainActivity.java
+++ app/src/main/java/com/example/commet/booker/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.commet.booker;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username_text;
+    private EditText pass_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
         final Button login_button = (Button) findViewById(R.id.login_button);
         final Button signup_button = (Button) findViewById(R.id.signup_button);
+        username_text = (EditText)findViewById(R.id.username);
+        pass_text = (EditText)findViewById(R.id.password);
 
         login_button.setOnClickListener( new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                login(v);
+                final String username = username_text.getText().toString();
+                final String password = String.valueOf(pass_text.getText());
+                login(v, username, password);
             }
         });
 
@@ -30,15 +37,36 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
                 Toast.makeText(getApplicationContext(), "You do not need to signup in this version.",
                         Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
 
-    public void login(View view){
-        Intent intent = new Intent(this, book_list.class);
-        startActivity(intent);
+    public void login(View view, String user, String pass){
+        if (checkUsername(user) && checkPassword(pass)) {
+            Intent intent = new Intent(this, book_list.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Invalid login credentials! Valid email required.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean checkUsername(String s) {
+        if (s.contains(String.valueOf('@')) == false) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkPassword(String s) {
+        if (s.length() < 8) {
+            return false;
+        }
+        return true;
     }
 }
