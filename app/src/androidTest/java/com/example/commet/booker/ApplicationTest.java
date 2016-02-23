e5233e4098e6a8ad3c493a6c3f1afe553bbbd0ff
Jesse Roe <jessedroe@gmail.com>
2016-02-23 10:55:23 -0500

    Added a Application test to validate the login button action.

--- app/src/androidTest/java/com/example/commet/booker/ApplicationTest.java
+++ app/src/androidTest/java/com/example/commet/booker/ApplicationTest.java
@@ -1,5 +1,7 @@
 package com.example.commet.booker;
 import android.app.Application;
+import android.content.ComponentName;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
@@ -11,8 +13,15 @@ import org.junit.Test;
 import org.junit.matchers.*;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 //import static java.util.regex.Pattern.matches;
 
@@ -24,10 +33,28 @@ import static android.support.test.espresso.matcher.ViewMatchers.*;
 public class ApplicationTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+    public IntentsTestRule<MainActivity> mActivityRule = new IntentsTestRule(MainActivity.class);
 
     @Test
-    public void listGoesOverTheFold() {
+    public void testForCorrectTitle() {
         onView(withText("Barter Books")).check(matches(withText("Barter Books")));
     }
+
+    @Test
+    public void testButtonClickLogin() {
+        onView(withId(R.id.username)).perform(clearText(), typeText("jesse@"));
+        onView(withId(R.id.password)).perform(clearText(), typeText("134567892"));
+        onView(withText("Barter Books")).perform(closeSoftKeyboard());
+        onView(withText("Login")).perform(click());
+        intended(hasComponent(new ComponentName(getTargetContext(), book_list.class)));
+    }
+
+//    @Test
+//    public void testButtonClickSignUp() {
+//        onView(withId(R.id.username)).perform(clearText(), typeText("jesse@"));
+//        onView(withId(R.id.password)).perform(clearText(), typeText("134567892"));
+//        onView(withText("Barter Books")).perform(closeSoftKeyboard());
+//        onView(withText("Signup")).perform(click());
+//        onView(withText("Barter Books")).check(matches(withText("You do not need to signup in this version.")));
+//    }
 }
