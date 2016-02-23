139ef3514ec9fc20792f184eb6eb01da98581bcb
Jesse Roe <jessedroe@gmail.com>
2016-02-23 13:51:16 -0500

    Create extra content in SigninActivity.

--- app/src/main/java/com/example/commet/booker/SignupActivity.java
+++ app/src/main/java/com/example/commet/booker/SignupActivity.java
@@ -1,13 +1,20 @@
 package com.example.commet.booker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private EditText username_text;
+    private EditText pass_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +22,13 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        final Button login_button = (Button) findViewById(R.id.login_button);
+        final Button signup_button = (Button) findViewById(R.id.signup_button);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        username_text = (EditText)findViewById(R.id.username);
+        pass_text = (EditText)findViewById(R.id.password);
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,7 +36,20 @@ public class SignupActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        signup_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                signup(v);
+            }
+        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void signup(View view){
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
 }
